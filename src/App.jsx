93b83e2c3c5c34b3f3038c10992b2af6589029a8
Jessica Maddux <jessicamaddux@Jessicas-Air.lan>
2024-02-25 00:54:14 -0600

    added new code to render puppy names

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
+import './App.css'
 
 
-      <div>
-        
+
+function App () {
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log("puppyList: ", puppyList);
+  return ( 
+      <div className="App">
+        {
+          puppies.map((puppy) => {
+            return <p>{puppy.name}</p>
+          })
+        }
       </div>
+  );
+
+    
+
+
+}
+
+export default App
       
