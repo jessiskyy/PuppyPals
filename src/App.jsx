9063f7c86eed6f4f9a9c68b83f2a1e19df175d72
Jessica Maddux <jessicamaddux@Jessicas-Air.lan>
2024-02-26 01:37:28 -0600

    added code for functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,45 @@
 import { useState } from 'react'
 import {puppyList} from './data.js'
 import './App.css'
+import './puppy.css'
 
 
 
 function App () {
-  const [puppies, setPuppies] = useState(puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
   console.log("puppyList: ", puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup);
+
   return ( 
+    
       <div className="App">
+          <h1>🐾 Puppy Pals 🐾</h1>
         {
           puppies.map((puppy) => {
-            return <p>{puppy.name}</p>
+            return <p onClick={()=>{
+              setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
+            
           })
         }
+        {featPupId && (
+        <div>
+          <h1>Puppy Details 📋</h1>
+          <h2 className="Name">{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
   );
 
-    
-
-
 }
+
+
 
 export default App
       
